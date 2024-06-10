e4c7afd21aacd33583a84c8d46ed2616f294daf8
Karim Naser <karim.n.1995@gmail.com>
2024-06-10 18:58:09 +0300

    gg

---
+++ Interfaces/INotPayedmoneyHistory.cs
@@ -0,0 +1,52 @@
+﻿using Cf_Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using X.PagedList;
+
+namespace Interfaces
+{
+    public interface INotPayedmoneyHistoryServess : IPaginationHelper<NotPayedmoneyHistoryVM> 
+    {
+
+
+        void SaveNotPayedmoney(NotPayedmoneyHistoryVM criteria);
+        void DeleteNotPayedmoney(int id);
+        IPagedList<NotPayedmoneyHistoryVM> SearchNotPayedmoney(NotPayedmoneyHistoryVM criteria);    
+        
+        
+        void SaveNotPayedmoneyHistoryDetails(int id  );
+        void DeleteNotPayedmoneyHistory(int id);
+        IPagedList<NotPayedmoneyHistoryVM> SearchNotPayedmoneyHistory(NotPayedmoneyHistoryVM criteria);
+    }
+
+
+
+
+    public interface IFinancialUserCashHistoryServess : IPaginationHelper<FinancialUserCashHistoryVM>
+    {
+
+
+        void SaveNotPayedmoney(FinancialUserCashHistoryVM criteria);
+        void DeleteNotPayedmoney(FinancialUserCashHistoryVM criteria);
+        IPagedList<FinancialUserCashHistoryVM> SearchNotPayedmoney(FinancialUserCashHistoryVM criteria);
+
+
+        void SaveNotPayedmoneyHistoryDetails(int id);
+        void DeleteNotPayedmoneyHistory(int id);
+        IPagedList<FinancialUserCashHistoryVM> SearchNotPayedmoneyHistory(FinancialUserCashHistoryVM criteria);
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Servess/NotPayedmoneyHistoryServess.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using C_Utilities;
+
+using Cf_Viewmodels;
+using DataAcessLayers;
+using Interfaces;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X.PagedList;
+
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Servess
+{
+    public class NotPayedmoneyHistoryServess : PaginationHelper<NotPayedmoneyHistoryVM>, INotPayedmoneyHistoryServess
+    {
+        public readonly ApplicationDBcontext _context; 
+        Imgoperation _Imgoperation;
+
+        private readonly IMapper _mapper;
+
+        public NotPayedmoneyHistoryServess(ApplicationDBcontext context, IMapper mapper    ,     Imgoperation imgoperation
+)
+        {
+            _Imgoperation = imgoperation;
+            _context = context;
+            _mapper = mapper;
+
+        }
+
+        public bool CheckIfExisit(productVM entity)
+        {
+            return _context.products.Any(i => i.Id != entity.Id && i.ProductName == entity.ProductName &&i.Price==entity.Price);
+        }
+
+        public void SaveNotPayedmoney(NotPayedmoneyHistoryVM criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNotPayedmoney(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPagedList<NotPayedmoneyHistoryVM> SearchNotPayedmoney(NotPayedmoneyHistoryVM criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveNotPayedmoneyHistoryDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNotPayedmoneyHistory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPagedList<NotPayedmoneyHistoryVM> SearchNotPayedmoneyHistory(NotPayedmoneyHistoryVM criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        //public IPagedList<productVM> Search(productVM criteria)
+        //{
+        //    var queryable = _context.products.Where(
+        //        product =>
+        //            (criteria.ProductName == null || product.ProductName.Contains(criteria.ProductName))
+        //            && (criteria.Description == null || product.Description.Contains(criteria.Description)))
+        //        .Select(i => new productVM
+        //        {
+        //            Id = i.Id,
+        //            ProductName = i.ProductName,
+        //            Description = i.Description,
+        //            Discount = i.Discount,
+        //            CategoryTyPe = (Enumes.CategoryType)i.CategoryTyPe,
+        //            Price = i.Price,
+        //            Qantity = i.Qantity,
+        //             //, CategoryName= i.Category.CategoryName,
+        //             CoverString= _context.ProductAttachments.Where(p=>p.ProductId==i.Id).OrderByDescending(i=>i.ProductId).FirstOrDefault().FilePath,
+        //        })
+        //        .OrderBy(g => g.Id);
+
+        //    // Provide a default value for PageNumber if it's null
+        //    int pageNumber = criteria.PageNumber ?? 1;
+
+        //    var pagedList = GetPagedData(queryable, pageNumber);
+
+        //    return pagedList;
+        //}
+
+
+
+
+    }
+}
